a51ddaf4319d84dea178d7e71e37e85c17b938a6
NguyenDuyHieu987 <104075342+NguyenDuyHieu987@users.noreply.github.com>
2025-01-08 21:54:15 +0700

    chore: update

--- src/middleware/loadUser.ts
+++ src/middleware/loadUser.ts
@@ -1,11 +1,12 @@
 import { useAuthStore } from '@/stores';
 // let isInitialLoad = true;
 
+const authStore = useAuthStore();
+
 export const loadUser = async (to, from, next) => {
   if (to.meta?.layout?.name != 'auth' && from.meta?.layout?.name != 'auth') {
-    const authStore = useAuthStore();
     if (from.matched?.length == 0 && !window.initialLoad) {
-      await authStore.loadUser();
+      authStore.loadUser();
       window.initialLoad = true;
     } else {
       if (!authStore.isLogin) next({ path: '/login' });
